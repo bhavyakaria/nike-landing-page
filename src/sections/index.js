63392f7dc89c66739265658b2efe87bd4e691b96
bhavyakaria <bhavyakaria@yahoo.in>
2023-10-14 22:02:09 +0530

    tailwind learning project

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Subscribe,
+    Footer,
+    SpecialOffer,
+    CustomerReviews,
+    Services
+};
